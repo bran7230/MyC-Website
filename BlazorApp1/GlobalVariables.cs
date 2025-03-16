95abba356512c90333bcf1ad931386a52799fb78
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-03-16 16:02:29 -0400

    now saves query

--- BlazorApp1/GlobalVariables.cs
+++ BlazorApp1/GlobalVariables.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using BlazorApp1.Components.Pages;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
@@ -13,13 +14,15 @@ namespace BlazorApp1
         public int count { get; set; }
         public string jsonstring { get; set; }
         public string last_time { get; set; }
+        public string url { get; set; }
+        public string query { get; set; }
 
         public GlobalVariables()
         {
             Name = "GlobalVariable";
-            Value = 1 + 1;
             ip = GetIp();
-            
+            url = String.Empty;
+            query = String.Empty; 
 
             try
             {
@@ -29,21 +32,16 @@ namespace BlazorApp1
                 if (File.Exists(filePath))
                 {
                     var jsonData = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(File.ReadAllText(filePath));
-                    count = jsonData != null && jsonData.ContainsKey("times_loaded")
-                                ? jsonData["times_loaded"].GetInt32()
-                                : 0;
+                   
                     // Always update the IP instead of using the stored value
                     ip = GetIp();
                 }
-               
-
                 else
                 {
-                    count = 0;
-                    jsonstring = JsonSerializer.Serialize(new { times_loaded = count, ip_address = ip, last_loaded = last_time });
+                    
+                    jsonstring = JsonSerializer.Serialize(new { times_loaded = count, ip_address = ip, last_loaded = last_time, query_saved = query });
                     File.WriteAllText(filePath, jsonstring);
                 }
-               
             }
             catch (Exception ex)
             {
