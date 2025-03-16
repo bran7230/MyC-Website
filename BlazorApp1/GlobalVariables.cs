02020edfb382c72f8c80089c25a7ecd9d5762a24
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-03-16 02:55:46 -0400

    Add project files.

---
+++ BlazorApp1/GlobalVariables.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace BlazorApp1
+{
+    public class GlobalVariables
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string ip { get; set; }
+        public int count { get; set; }
+        public string jsonstring { get; set; }
+        public string last_time { get; set; }
+
+        public GlobalVariables()
+        {
+            Name = "GlobalVariable";
+            Value = 1 + 1;
+            ip = GetIp();
+            
+
+            try
+            {
+                // Create an absolute file path in the current directory.
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "data.json");
+
+                if (File.Exists(filePath))
+                {
+                    var jsonData = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(File.ReadAllText(filePath));
+                    count = jsonData != null && jsonData.ContainsKey("times_loaded")
+                                ? jsonData["times_loaded"].GetInt32()
+                                : 0;
+                    // Always update the IP instead of using the stored value
+                    ip = GetIp();
+                }
+               
+
+                else
+                {
+                    count = 0;
+                    jsonstring = JsonSerializer.Serialize(new { times_loaded = count, ip_address = ip, last_loaded = last_time });
+                    File.WriteAllText(filePath, jsonstring);
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving to data.json: {ex.Message}");
+            }
+        }
+
+        public static string GetIp()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var address in host.AddressList)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return address.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,36 @@
+using BlazorApp1.Components;
+using BlazorApp1;
+using Microsoft.VisualBasic;
+using System.Xml.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddSingleton<GlobalVariables>();
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
